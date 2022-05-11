9451d6d19d1ef1a9ec1213ce74e2f6a4103b32b4
Joshua Alana <alanajoshua535@gmail.com>
2022-05-11 18:16:33 +1400

    final commit on frontend

--- src/pages/list/List.jsx
+++ src/pages/list/List.jsx
@@ -5,6 +5,7 @@ import { useLocation } from 'react-router-dom';
 import { useState } from 'react';
 import { format } from 'date-fns'
 import { DateRange } from 'react-date-range';
+import SearchItem from '../../components/searchItem/SearchItem';
 
 
 export default function List() {
@@ -66,7 +67,17 @@ export default function List() {
                         </div>
                         <button>Search</button>
                     </div>
-                    <div className='listResult'></div>
+                    <div className='listResult'>
+                        <SearchItem />
+                        <SearchItem />
+                        <SearchItem />
+                        <SearchItem />
+                        <SearchItem />
+                        <SearchItem />
+                        <SearchItem />
+                        <SearchItem />
+
+                    </div>
 
                 </div>
             </div>
